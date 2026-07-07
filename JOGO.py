a5f8fcafd234ffd0056c5393e77de634b79cc150
Lucas Gonçalves de Azevedo <goncalvesdeazevedolucas@gmail.com>
2026-07-07 14:00:03 -0300

    Update JOGO.py

--- JOGO.py
+++ JOGO.py
@@ -1,8 +1,19 @@
 import random
 import time
 import math
+import msvcrt
 
 def afk():
-    xp_afk = -1
+    xp_afk= 0
+    print("Aperte Qualquer Tecla para Parar")
     while True:
-        break
+        xp_afk += random.randint( 10, 20)
+        time.sleep(1)
+        print(f"XP Acumulado: {xp_afk}")
+        if msvcrt.kbhit():
+            msvcrt.getch()
+            return xp_afk
+            break
+
+xp= 0
+xp += afk()
