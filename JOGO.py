cabf1df7c34f58485954d2d3185b89170e294bf3
Lucas Gonçalves de Azevedo <goncalvesdeazevedolucas@gmail.com>
2026-07-13 14:19:00 -0300

    Classes

--- JOGO.py
+++ JOGO.py
@@ -2,6 +2,7 @@ import random
 import time
 import math
 import msvcrt
+import unicodedata
 
 def afk():
     xp_afk= 0
@@ -45,7 +46,10 @@ def modo(choose):
     else:
         choose = start()
         return modo(choose)
-      
+def noacent(palavra):        #https://www.youtube.com/watch?v=ZPqb8k76sK4&t=437s
+    nfdk_normalizacao= unicodedata.normalize('NFKD', palavra)
+    return ''.join([x for x in nfdk_normalizacao if not unicodedata.combining(x)])
+
 inventario= list()
 xp = 0
 vida = random.randint(15,20)
@@ -60,12 +64,39 @@ if comeco == "AFK":
     jornada()
 elif comeco == "JORNADA":
     nome = input("Insira o Nome do seu Personagem: ")
-    classe = input("Escolha sua Classe [1- Médico ], [ 2- Sobrevivente], [3- Idiota] :")
-    if classe == "1" or classe.upper() == "MEDICO" or "MÉDICO" or "MÈDICO":
-        vida += 5
-        inventario.append("Medkit")
-    jornada()
-
+    if nome == "":
+        nome = "???"
+    while True:
+        classe = input("Escolha sua Classe [1- Médico ], [ 2- Sobrevivente], [3- Idiota] :")
+        if classe == "1" or noacent(classe).upper() == "MEDICO":
+            classe = "MÉDICO"
+            vida += 5
+            inventario.append("Medkit")
+            jornada()
+            break
+        elif classe == "2" or noacent(classe).upper() == "SOBREVIVENTE":
+            classe= "SOBREVIVENTE"
+            vida += 25
+            break
+        elif classe == "3" or noacent(classe).upper() == "IDIOTA":
+            classe = "IDIOTA"
+            sorte += -20
+            break
+        elif classe == "2026302743":
+            classe= "ADMIM"
+            vida = 2000000000
+            dano = 2000000000
+            sorte = 100
+            level = 100
+            inventario.append("?")
+            chamar_status(xp, vida, sorte, dano, level,)
+            break
+        else:
+            continue
+    
+print(nome)
+print(classe)
+print(inventario)  
 
 
 
