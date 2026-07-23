6d4ece3138b14ee5b0b63accdbaae0c6df423d1c
Lucas Gonçalves de Azevedo <goncalvesdeazevedolucas@gmail.com>
2026-07-23 14:30:39 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,7 +1,12 @@
 import unicodedata
-def noacent(palavra):        #https://www.youtube.com/watch?v=ZPqb8k76sK4&t=437s
-    nfdk_normalizacao= unicodedata.normalize('NFKD', palavra)
-    return ''.join([x for x in nfdk_normalizacao if not unicodedata.combining(x)]).upper()
+# def noacent(palavra):        #https://www.youtube.com/watch?v=ZPqb8k76sK4&t=437s
+#     nfdk_normalizacao= unicodedata.normalize('NFKD', palavra)
+#     return ''.join([x for x in nfdk_normalizacao if not unicodedata.combining(x)]).upper()
 
-print(noacent("médiCO23"))
+# print(noacent("médiCO23"))
+import time
+import os
 
+print("LASJHBHUBYGHNCUHTJ$RSVBH DN")
+time.sleep(2)
+os.system("cls")
